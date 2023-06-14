106757f861260c5347406634e558b741d9cfa1d8
Mahyuddin Susanto <30291+udienz@users.noreply.github.com>
2023-06-14 16:14:25 +0700

    Add more records (#7)
    * Add more records
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- dns1/resources.tf
+++ dns1/resources.tf
@@ -6,3 +6,163 @@ resource "dns_a_record_set" "www" {
     "127.0.0.53"
   ]
 }
+
+resource "dns_a_record_set" "subdomain_1_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-1"
+  addresses = [
+    "100.64.1.1",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_2_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-2"
+  addresses = [
+    "100.64.1.2",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_3_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-3"
+  addresses = [
+    "100.64.1.3",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_4_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-4"
+  addresses = [
+    "100.64.1.4",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_5_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-5"
+  addresses = [
+    "100.64.1.5",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_6_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-6"
+  addresses = [
+    "100.64.1.6",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_7_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-7"
+  addresses = [
+    "100.64.1.7",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_8_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-8"
+  addresses = [
+    "100.64.1.8",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_9_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-9"
+  addresses = [
+    "100.64.1.9",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_10_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-10"
+  addresses = [
+    "100.64.1.10",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_11_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-11"
+  addresses = [
+    "100.64.1.11",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_12_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-12"
+  addresses = [
+    "100.64.1.12",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_13_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-13"
+  addresses = [
+    "100.64.1.13",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_14_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-14"
+  addresses = [
+    "100.64.1.14",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_15_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-15"
+  addresses = [
+    "100.64.1.15",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_16_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-16"
+  addresses = [
+    "100.64.1.16",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_17_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-17"
+  addresses = [
+    "100.64.1.17",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_18_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-18"
+  addresses = [
+    "100.64.1.18",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_19_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-19"
+  addresses = [
+    "100.64.1.19",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_20_dns1_lab" {
+  zone = "dns1.lab."
+  name = "subdomain-20"
+  addresses = [
+    "100.64.1.20",
+  ]
+}
---
+++ dns2/resources.tf
@@ -0,0 +1,168 @@
+resource "dns_a_record_set" "www_dns2_lab" {
+  zone = "dns2.lab."
+  name = "www"
+  addresses = [
+    "127.0.0.1",
+    "127.0.0.53"
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_1_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-1"
+  addresses = [
+    "100.64.2.1",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_2_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-2"
+  addresses = [
+    "100.64.2.2",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_3_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-3"
+  addresses = [
+    "100.64.2.3",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_4_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-4"
+  addresses = [
+    "100.64.2.4",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_5_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-5"
+  addresses = [
+    "100.64.2.5",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_6_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-6"
+  addresses = [
+    "100.64.2.6",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_7_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-7"
+  addresses = [
+    "100.64.2.7",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_8_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-8"
+  addresses = [
+    "100.64.2.8",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_9_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-9"
+  addresses = [
+    "100.64.2.9",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_10_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-10"
+  addresses = [
+    "100.64.2.10",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_11_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-11"
+  addresses = [
+    "100.64.2.11",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_12_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-12"
+  addresses = [
+    "100.64.2.12",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_13_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-13"
+  addresses = [
+    "100.64.2.13",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_14_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-14"
+  addresses = [
+    "100.64.2.14",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_15_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-15"
+  addresses = [
+    "100.64.2.15",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_16_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-16"
+  addresses = [
+    "100.64.2.16",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_17_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-17"
+  addresses = [
+    "100.64.2.17",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_18_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-18"
+  addresses = [
+    "100.64.2.18",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_19_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-19"
+  addresses = [
+    "100.64.2.19",
+  ]
+}
+
+resource "dns_a_record_set" "subdomain_20_dns2_lab" {
+  zone = "dns2.lab."
+  name = "subdomain-20"
+  addresses = [
+    "100.64.2.20",
+  ]
+}
